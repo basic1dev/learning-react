2075d554f41e766a67b0ffce0309d1a15829830b
basic1dev <jeromegennings@gmail.com>
2024-01-31 19:27:08 +0530

    add multiple item select

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -2,12 +2,14 @@ import { Box, TextField, MenuItem } from "@mui/material";
 import React, {useState} from "react";
 
 export const MuiSelect = () => {
-    const [country, setCountry] = useState('');
+    const [country, setCountries] = useState<string[]>([]);
     console.log({country})
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCountry(event.target.value as string)
+        const value = event.target.value
+        setCountries(typeof value === 'string' ? value.split(',') : value)
     }
+
   return (
     <Box width='250px'>
         <TextField
@@ -16,6 +18,9 @@ export const MuiSelect = () => {
             value={country}
             onChange={handleChange}
             fullWidth
+            SelectProps={{
+                multiple: true,
+            }}
         >
             <MenuItem value="SL">Sri Lanka</MenuItem>
             <MenuItem value="CA">Canada</MenuItem>
